bccd211a09bb3891d3d88c8fd3eae06c10fc3505
Yunes El Hamad <yunes_elhamad@sluz.ch>
2024-04-22 14:15:38 +0200

    Created Class Car and added Member variables

---
+++ RentCars/RentCars/Model/Car.cs
@@ -0,0 +1,14 @@
+﻿namespace RentCars.Model
+{
+    public class Car
+    {
+        public int CID { get; set; }
+        public string CarName { get; set; }
+        public string CarBrand { get; set; }
+        public int Seats { get; set; }
+        public int Doors { get; set; }
+        public bool IsAutomatic { get; set; }
+        public double HorsePwr { get; set; }
+
+    }
+}
