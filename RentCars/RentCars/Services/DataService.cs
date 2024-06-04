158652e5b29699e063f70270ab495a98726bba5c
thayuyu <yunes1703@icloud.com>
2024-06-04 21:13:27 +0200

    finished functionality in Checkout. Added a check if user is logged in in CarShow and added the possibility to log out. Added possibility to go back to CarShow in CarPurchase, deleted unused variables and added a check if the user chose a plan. Created the final page "Result". Modified DataService to share more data through pages.

--- RentCars/RentCars/Services/DataService.cs
+++ RentCars/RentCars/Services/DataService.cs
@@ -5,13 +5,22 @@ namespace RentCars.Services
     public class DataService
     {
         public int SharedUserID { get; set; }
-        public string Plan {  get; set; }
+        public string Plan { get; set; }
         public List<Addition> Additions { get; set; }
-
+        public int Days { get; set; }
+        public string Paymentmethod { get; set; }
 
         public DataService() 
         { 
             Additions = new List<Addition>();
+        }
+
+        public void ClearRentData()
+        {
+            Additions.Clear();
+            Days = 0;
+            Plan = null;
+            Paymentmethod = null;
         }
     }
 }
